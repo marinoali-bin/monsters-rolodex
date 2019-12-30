2b2a0610499f06b4aae7e1c42a8d7da996095fc4
Marino Ali-Bin <marinoali-bin@macbookpro-mab.local>
2019-12-30 03:08:25 +0100

    Updates

---
+++ precache-manifest.c9b48cd1ba6e75acc6aa4217c7ba4e69.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56f20bd1fe5b89cbdf52124faea8d089",
+    "url": "marinoali-bin.github.io/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "6a5eb4350774350957fa",
+    "url": "marinoali-bin.github.io/monsters-rolodex/static/css/main.42263de8.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "marinoali-bin.github.io/monsters-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "marinoali-bin.github.io/monsters-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6a5eb4350774350957fa",
+    "url": "marinoali-bin.github.io/monsters-rolodex/static/js/main.e6659572.chunk.js"
+  },
+  {
+    "revision": "e262e5ffae9bd74676e7",
+    "url": "marinoali-bin.github.io/monsters-rolodex/static/js/runtime-main.51afae76.js"
+  }
+]);
